1bcb4d0dd514e8f581db9323aad3fa71bc4e8d15
Dilshod <111531709+dilshod1405@users.noreply.github.com>
2023-12-07 21:15:19 +0500

    Update Main.js

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -16,12 +16,12 @@ export default class Main extends Component {
                     <h5>{this.props.main('main.card_title1')}</h5>
                     <p style={{color: '#1C282B'}}>{this.props.main('main.card_text1')}</p>
                 </div>
-                <div className="mycard" style={{color: element.text}} data-aos='fade-up-right' data-aos-duration="1000">
+                <div className="mycard" style={{color: element.text}} data-aos='fade-up' data-aos-duration="1000">
                     <lord-icon src='https://cdn.lordicon.com/jgnvfzqg.json' trigger="loop" style={{width: '50px', height: '50px', }} colors="primary:#1c282b" state='in-bookmark' delay='1000'></lord-icon>
                     <h5>{this.props.main('main.card_title2')}</h5>
                     <p style={{color: '#1C282B'}}>{this.props.main('main.card_text2')}</p>
                 </div>
-                <div className="mycard" style={{color: element.text}} data-aos='fade-up-right' data-aos-duration="1000">
+                <div className="mycard" style={{color: element.text}} data-aos='fade-up-left' data-aos-duration="1000">
                     <lord-icon src='https://cdn.lordicon.com/mjcariee.json' trigger="loop" style={{width: '50px', height: '50px', }} colors="primary:#1c282b" state='in-bookmark' delay='1000'></lord-icon>
                     <h5>{this.props.main('main.card_title3')}</h5>
                     <p style={{color: '#1C282B'}}>{this.props.main('main.card_text3')}</p>
