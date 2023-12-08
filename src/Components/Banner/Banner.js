6966146b6285ee6375adc702367846f94a987da6
Dilshod <111531709+dilshod1405@users.noreply.github.com>
2023-12-08 11:53:56 +0500

    Update Banner.js

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -14,8 +14,6 @@ export default class Banner extends Component {
                 <p data-aos="fade-up" data-aos-duration="3000"><span style={{color: element.text}} > &#8213; </span> {this.props.banner('banner.title')}</p>
                 <br />
                 <a href={'bio'} data-aos="fade-right" data-aos-duration="2000"> <strong>{this.props.banner('banner.link1')}</strong></a>
-                <a href={'https://tashkent.hh.uz/resume_converter/Normurodov%20Dilshod.pdf?hash=9f984d71ff0ca4c0d70039ed1f7a72366f3531&type=pdf&hhtmSource=resume&hhtmFrom=resume_list'} data-aos="fade-right" data-aos-duration="2000" style={{marginLeft: '2%'}}> <strong>Frontend resume</strong></a>
-                <a href={'https://tashkent.hh.uz/resume_converter/Normurodov%20Dilshod.pdf?hash=f30736c9ff0ca4c78b0039ed1f796e46655247&type=pdf&hhtmSource=resume&hhtmFrom=resume_list'} data-aos="fade-right" data-aos-duration="2000" style={{marginLeft: '2%'}}> <strong>Backend resume</strong></a>
             </div>
             <img src={element.photo} alt="" data-aos="zoom-out" data-aos-duration="1500"/>
         </div>
